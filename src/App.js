11224f451e2d324e14f527357115e2c56d6e9fd0
Christopher-Deo <chris@christopherdeo.com>
2023-01-31 17:28:15 -0600

    Created pages and added routes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+//importing pages
+import Home from './pages/Home';
+import ForgotPassword from './pages/ForgotPassword';
+import Offer from './pages/Offer';
+import Profile from './pages/Profile';
+import LogIn from './pages/LogIn';
+import Register from './pages/Register';
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/forgotpassword" element={<ForgotPassword />} />
+          <Route path="/offer" element={<Offer />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/login" element={<LogIn />} />
+          <Route path="/register" element={<Register/>} />
+        </Routes>     
+      </Router>
+    </>
   );
 }
 
